0a6b4e84da2a9a6a217f1904ca5c8496591dff8d
okayblue <56899411+okayblue@users.noreply.github.com>
2022-08-18 19:55:42 -0400

    Add main game functions

--- lib/main.rb
+++ lib/main.rb
@@ -6,9 +6,94 @@ end
 
 class Board
   include Dictionary
-  attr_accessor :secret_word
+  attr_accessor :secret_word, :hint, :incorrect_guesses, :print
 
   def initialize
     @secret_word = WORDS.sample
+    @hint = []
+    @incorrect_guesses = 0
+  end
+
+  def initial_hint
+    secret_word.split('').each { |letter| hint.push('_') }
+  end
+
+  def print
+    puts "#{hint.join(' ')}\n\n"
+  end
+
+  def guess_check(guess)
+    if secret_word.include?(guess)
+      puts 'Correct'
+      secret_word.split('').each_with_index do |letter, index|
+        if letter == guess
+          self.hint[index] = guess
+        end
+      end
+    elsif !secret_word.include?(guess)
+      puts "Incorrect"
+      self.incorrect_guesses += 1
+    end
   end
 end
+
+class Player
+  attr_accessor :guessed_letters, :guess
+
+  def initialize
+    @guess = ''
+    @guessed_letters = []
+  end
+
+  def make_guess
+    puts 'Guess a letter:'
+    current_guess = ''
+
+    until current_guess.match /\A[a-zA-Z'-]{1}\z/
+      current_guess = gets.chomp.downcase
+    end
+
+    guessed_letters.push(current_guess)
+    self.guess = current_guess
+  end
+end
+
+class Game
+  attr_accessor :player, :board
+  def initialize
+    @player = Player.new
+    @board = Board.new
+  end
+
+  def game_over_check
+    game_over = false
+    if board.incorrect_guesses == 6
+      puts "You ran out of guesses. The word was #{board.secret_word}"
+      game_over = true
+    elsif board.hint.join('') == board.secret_word
+      puts "You win lol"
+      game_over = true
+    end
+    game_over
+  end
+
+  def instructions
+    puts "Welcome to hangman! Guess the letters, it's game over if you get 6 wrong letters."
+    board.initial_hint
+    board.print
+  end
+
+  def play
+    instructions
+    
+    until game_over_check == true
+      puts "Letters guessed so far: #{player.guessed_letters}\n\n"
+      player.make_guess
+      board.guess_check(player.guess)
+      board.print
+    end
+  end
+end
+
+game = Game.new
+game.play
